8476cb86a6221abcab502e410ddee7a14d79eb0b
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-07-12 10:51:32 -0400

    🔥 Chore: Added description

--- src/components/History/components/HistoryPre.tsx
+++ src/components/History/components/HistoryPre.tsx
@@ -8,15 +8,15 @@ interface HistoryProps {}
 
 export const History: React.FC<HistoryProps> = ({}) => {
   return (
-    <div className="h-full flex items-center pl-5">
-      <div className="relative top-0 left-20">
+    <div className="h-full flex items-center pr-5 pl-5">
+      <div className="relative top-0 left-0">
         <img
-          className="relative top-0 left-0 rounded"
+          className="relative top-0 left-0"
           src={preconstruction1826}
           alt="Pre construction 1826"
         />
         <History__boat
-          className="transition delay-75 absolute top-0 left-0"
+          className="absolute top-0 left-0"
           src={preconstruction1826boat}
         />
         <History__stone
@@ -24,30 +24,16 @@ export const History: React.FC<HistoryProps> = ({}) => {
           src={preconstruction1826stone}
         />
       </div>
-      <Center__desc className="absolute top-1/2 left-2/3 flex-col bg-gray-600 bg-opacity-30 p-5 rounded">
-        <p className="text-2xl font-medium text-gray-100 pb-4">
+      <Center__desc className="absolute top-1/2 left-1/2 flex-col">
+        <p className="text-2xl font-medium text-yellow-600 pb-4">
           Pre Construction, 1826-1858
         </p>
-        <p className="text-lg text-gray-200 text pb-5">
+        <p className="text-lg text-yellow-500 text">
           In 1826, Lieutenant-Colonel John By of the Royal Engineers begins to
           design and build the Rideau Canal. This navigable waterway will
           connect the Ottawa River in the north with Lake Ontario in the south.
           It will provide a secure shipping route between Montréal, Quebec, and
           Kingston, Ontario. The northern post of the canal is named Bytown.
-        </p>
-        <p className="text-lg text-gray-200 text pb-5">
-          On January 1, 1855, Bytown officially incorporates as a city and
-          rename­d Ottawa. With a population of almost 7,800, the city borders
-          English and French Canada. Just 14 years earlier, Upper and Lower
-          Canada joined to become the United Province of Canada.
-        </p>
-        <p className="text-lg text-gray-200 text">
-          On December 31, 1857, Queen Victoria is asked to select a location for
-          the permanent capital of the United Province of Canada. ­­At the time,
-          the capital alternated between Toronto and Québec every four years.
-          The Queen selects Ottawa because it sits far from the American border
-          and it is situated on a cliff, making it easier to defend from a
-          possible attack.
         </p>
       </Center__desc>
     </div>
@@ -111,5 +97,5 @@ const History__stone = styled.img`
 `;
 
 const Center__desc = styled.div`
-  transform: translate(-35%, -50%);
+  transform: translate(-50%, -50%);
 `;
